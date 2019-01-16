16daf7926ea646d849e08ec288f23a06c88429de
Jacob Farkas <jfarkas@lrei.org>
2019-01-16 14:34:28 -0500

    Update sketch.js

--- dice/sketch.js
+++ dice/sketch.js
@@ -4,21 +4,23 @@ function setup() {
 }
 
 function draw() {
+  
+  // mouse coordinates (useful when creating shapes, comment out once shapes are all drawn)
   textSize(14);
   fill(255);
-  
   text('X: ' + Math.round(mouseX) + ' Y: ' + Math.round(mouseY), 2, 10);
 	
-  strokeWeight(3);
+  /* Top Dice - Square */
+	strokeWeight(3);
 	stroke('black');
 	fill(255);
 	rect(20, 20, 200, 200);
 	fill('blue');
   
-	/* Roll a one for top dice */
+  /* Top Dice - ONE */
   /* ellipse(120, 120, 50, 50);*/
 
-  /* Roll a six for top dice */
+   /* Top Dice - SIX */
   ellipse(56, 80 , 50, 50);
   ellipse(120, 80, 50, 50);
   ellipse(186, 80, 50, 50);
@@ -26,12 +28,13 @@ function draw() {
   ellipse(120, 155, 50, 50);
   ellipse(186, 155, 50, 50);
   
+  /* Bottom Dice - Square */
 	fill(255);
 	rect(20, 250, 200, 200);
 	fill('blue');
 	
-  /* Roll a three for bottom dice */
+   /* Bottom Dice - THREE */
   ellipse(70, 300, 50, 50);
-	ellipse(120, 350, 50, 50);
-	ellipse(170, 400, 50, 50);
+  ellipse(120, 350, 50, 50);
+  ellipse(170, 400, 50, 50);
 }
